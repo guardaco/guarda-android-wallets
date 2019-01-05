92e6e9e3b2cc3dc46e40478071f86b735b7217f1
guardawallet <pavelhalcon@gmail.com>
2019-01-05 15:15:48 +0300

    dgb: own explorer

--- app/src/main/java/com/guarda/ethereum/models/constants/DgbExplorer.java
+++ app/src/main/java/com/guarda/ethereum/models/constants/DgbExplorer.java
@@ -5,6 +5,7 @@ public interface DgbExplorer {
 
     //String TRANSACTION_DETAILS = "https://blockchain.info/tx/";
     //String BLOCKCHAIN_INFO_KEY = "8e83fce3-96fb-4334-8078-34c45971cf0c";
-    String DGB_EXPLORER_BASE_URL = "https://digiexplorer.info/api/";
+//    String DGB_EXPLORER_BASE_URL = "https://digiexplorer.info/api/";
+    String DGB_EXPLORER_BASE_URL = "https://digibyteblockexplorer.com/api/";
 //    String DGB_EXPLORER_BASE_URL = "https://explorer-2.us.digibyteservers.io/api/";
 }
