5c83d2d1a46d2e73cbf714bdead5fb10bf1165fb
guardawallet <pavelhalcon@gmail.com>
2019-06-07 14:23:03 +0300

    zec: AuthorizationTypeActivity - don't start sync

--- app/src/main/java/com/guarda/ethereum/views/activity/AuthorizationTypeActivity.java
+++ app/src/main/java/com/guarda/ethereum/views/activity/AuthorizationTypeActivity.java
@@ -61,7 +61,7 @@ public class AuthorizationTypeActivity extends SimpleTrackOnStopActivity {
         walletManager.clearWallet();
         sharedManager.setIsShowBackupAlert(true);
 
-        syncManager.startSync();
+//        syncManager.startSync();
     }
 
     @Override
